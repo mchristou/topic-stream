3a01cdd9a25041f75e9808620011de9cee248b2e
chr.m <chri.marios@gmail.com>
2025-03-07 13:42:27 +0200

    Create multi topic receiver

--- src/fastpub.rs
+++ src/fastpub.rs
@@ -1,13 +1,15 @@
 use dashmap::DashMap;
+use futures::future::select_all;
 use std::{hash::Hash, sync::Arc};
 use tokio::sync::broadcast;
 
-pub struct FastPub<T: Eq + Hash, M: Clone> {
+#[derive(Debug)]
+pub struct FastPub<T: Eq + Hash + Clone, M: Clone> {
     subscribers: Arc<DashMap<T, broadcast::Sender<M>>>,
     capacity: usize,
 }
 
-impl<T: Eq + Hash, M: Clone> FastPub<T, M> {
+impl<T: Eq + Hash + Clone, M: Clone> FastPub<T, M> {
     pub fn new(capacity: usize) -> Self {
         Self {
             subscribers: Arc::new(DashMap::new()),
@@ -15,18 +17,55 @@ impl<T: Eq + Hash, M: Clone> FastPub<T, M> {
         }
     }
 
-    pub fn subscribe(&self, topic: T) -> broadcast::Receiver<M> {
-        let entry = self
-            .subscribers
-            .entry(topic)
-            .or_insert_with(|| broadcast::channel(self.capacity).0);
+    pub fn subscribe(&self, topics: &[T]) -> MultiTopicReceiver<M> {
+        let receivers: Vec<_> = topics
+            .iter()
+            .map(|topic| {
+                // Check if the topic exists in the map, and if not, insert a new sender
+                let sender = self
+                    .subscribers
+                    .entry(topic.clone())
+                    .or_insert_with(|| broadcast::channel(self.capacity).0); // Create a new channel if topic doesn't exist
 
-        entry.subscribe()
+                // Subscribe to the sender and get the receiver for this topic
+                sender.subscribe()
+            })
+            .collect();
+
+        MultiTopicReceiver::new(receivers)
     }
 
     pub fn publish(&self, topic: &T, message: M) {
         if let Some(sender) = self.subscribers.get(topic) {
             let _ = sender.send(message);
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct MultiTopicReceiver<M: Clone> {
+    receivers: Vec<broadcast::Receiver<M>>,
+}
+
+impl<M: Clone> MultiTopicReceiver<M> {
+    pub fn new(receivers: Vec<broadcast::Receiver<M>>) -> Self {
+        Self { receivers }
+    }
+
+    pub async fn recv(&mut self) -> Option<M> {
+        // Collect all receiver futures into a vector
+        let futures = self
+            .receivers
+            .iter_mut()
+            .map(|receiver| Box::pin(receiver.recv()))
+            .collect::<Vec<_>>();
+
+        // Use select_all to select the first resolved future
+        let (result, _index, _remaining) = select_all(futures).await;
+
+        match result {
+            Ok(m) => Some(m), // Return the first message received
+            Err(_) => None,   // If all receivers error out, return None
         }
     }
 }
@@ -47,14 +86,14 @@ mod tests {
         let fast_sub = FastPub::<Topic, Message>::new(2);
         let topic = Topic("test_topic".to_string());
 
-        // Subscriber 1 subscribes to the topic
-        let mut receiver = fast_sub.subscribe(topic.clone());
+        // Subscriber subscribes to the topic
+        let mut receiver = fast_sub.subscribe(&[topic.clone()]);
 
         // Publisher sends a message to the topic
         let message = Message("Hello, Subscriber!".to_string());
         fast_sub.publish(&topic, message.clone());
 
-        // Subscriber 1 receives the message
+        // Subscriber receives the message
         let received_message = receiver.recv().await.unwrap();
         assert_eq!(received_message, message);
     }
@@ -65,9 +104,9 @@ mod tests {
         let topic = Topic("test_topic".to_string());
 
         // Subscriber 1 subscribes to the topic
-        let mut receiver1 = fast_sub.subscribe(topic.clone());
+        let mut receiver1 = fast_sub.subscribe(&[topic.clone()]);
         // Subscriber 2 subscribes to the topic
-        let mut receiver2 = fast_sub.subscribe(topic.clone());
+        let mut receiver2 = fast_sub.subscribe(&[topic.clone()]);
 
         // Publisher sends a message to the topic
         let message = Message("Hello, Subscribers!".to_string());
@@ -87,7 +126,9 @@ mod tests {
         let fast_sub = FastPub::<Topic, Message>::new(2);
         let topic = Topic("test_topic".to_string());
 
-        let mut receiver = fast_sub.subscribe(Topic("invalid_topic".to_string()));
+        // Subscriber subscribes to a non-existent topic
+        let mut receiver = fast_sub.subscribe(&[Topic("invalid_topic".to_string())]);
+
         // Publisher sends a message to the topic with no subscribers
         let message = Message("Hello, World!".to_string());
         fast_sub.publish(&topic, message.clone());
@@ -113,8 +154,8 @@ mod tests {
         let fast_sub = FastPub::<Topic, Message>::new(2);
         let topic = Topic("test_topic".to_string());
 
-        // Subscriber 1 subscribes to the topic
-        let mut receiver = fast_sub.subscribe(topic.clone());
+        // Subscriber subscribes to the topic
+        let mut receiver = fast_sub.subscribe(&[topic.clone()]);
 
         // Publisher sends multiple messages
         let message1 = Message("Message 1".to_string());
@@ -122,11 +163,11 @@ mod tests {
         fast_sub.publish(&topic, message1.clone());
         fast_sub.publish(&topic, message2.clone());
 
-        // Subscriber 1 receives the first message
+        // Subscriber receives the first message
         let received_message1 = receiver.recv().await.unwrap();
         assert_eq!(received_message1, message1);
 
-        // Subscriber 1 receives the second message
+        // Subscriber receives the second message
         let received_message2 = receiver.recv().await.unwrap();
         assert_eq!(received_message2, message2);
     }
@@ -137,7 +178,7 @@ mod tests {
         let topic = Topic("test_topic".to_string());
 
         // Subscriber subscribes to the topic
-        let mut receiver = fast_sub.subscribe(topic.clone());
+        let mut receiver = fast_sub.subscribe(&[topic.clone()]);
 
         // Publisher 1 sends a message
         let message1 = Message("Message from Publisher 1".to_string());
@@ -163,7 +204,7 @@ mod tests {
         let topic2 = Topic("test_topic_2".to_string());
 
         // Subscriber subscribes to topic 1
-        let mut receiver1 = fast_sub.subscribe(topic1.clone());
+        let mut receiver1 = fast_sub.subscribe(&[topic1.clone()]);
 
         // Publisher sends a message to topic 1
         let message1 = Message("Hello, Topic 1".to_string());
@@ -174,7 +215,7 @@ mod tests {
         assert_eq!(received_message1, message1);
 
         // Subscriber subscribes to topic 2
-        let mut receiver2 = fast_sub.subscribe(topic2.clone());
+        let mut receiver2 = fast_sub.subscribe(&[topic2.clone()]);
 
         // Publisher sends a message to topic 2
         let message2 = Message("Hello, Topic 2".to_string());
@@ -183,5 +224,37 @@ mod tests {
         // Subscriber 2 receives the message for topic 2
         let received_message2 = receiver2.recv().await.unwrap();
         assert_eq!(received_message2, message2);
+    }
+
+    #[tokio::test]
+    async fn test_single_receiver_multiple_topics() {
+        let fast_sub = FastPub::<Topic, Message>::new(2);
+
+        // Define multiple topics
+        let topic1 = Topic("test_topic_1".to_string());
+        let topic2 = Topic("test_topic_2".to_string());
+        let topic3 = Topic("test_topic_3".to_string());
+
+        // Subscriber subscribes to multiple topics
+        let mut receiver = fast_sub.subscribe(&[topic1.clone(), topic2.clone(), topic3.clone()]);
+
+        // Publisher sends messages to each topic
+        let message1 = Message("Message for Topic 1".to_string());
+        let message2 = Message("Message for Topic 2".to_string());
+        let message3 = Message("Message for Topic 3".to_string());
+
+        fast_sub.publish(&topic1, message1.clone());
+        fast_sub.publish(&topic2, message2.clone());
+        fast_sub.publish(&topic3, message3.clone());
+
+        // Subscriber should receive the messages in the order they were published
+        let received_message1 = receiver.recv().await.unwrap();
+        assert_eq!(received_message1, message1);
+
+        let received_message2 = receiver.recv().await.unwrap();
+        assert_eq!(received_message2, message2);
+
+        let received_message3 = receiver.recv().await.unwrap();
+        assert_eq!(received_message3, message3);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 mod fastpub;
 
-pub use fastpub::FastPub;
+pub use fastpub::*;
